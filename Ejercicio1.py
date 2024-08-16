5b9a0bd50c452cf98cfff798787264ee4fa407b9
Tu Nombre <tuemail@example.com>
2024-08-16 15:04:39 -0600

    act

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -13,6 +13,7 @@ class veterinaria():
         self.estado= "ATENDIDO"
         return self.estado
     #funcion que almacena los datos en las variables
+    print("****************************************")
     def DatosPerro(self, nombreP,edadP, sexoP,
                    razaP,pesoP,estP):
         self.nombre=nombreP
@@ -25,35 +26,43 @@ class veterinaria():
             self.tamaño= "Perro Pequeño"
         else:
             self.tamaño= "Perro Grande"
+    
+
+        
     #funcion que captura lo que ingresa el usuario
     def RecibeDatosPerro(self):
-
         print("****************************************")
-        nombreP=input("Nombre del perro:")
-        edadP=int(input("Edad del perro en años:"))
-        sexoP=input("Sexo del perro:")
-        razaP=input("Raza del perro:")
-        pesoP= float(input("Peso en kg:"))
-        estP=input("Esterización (S/N):")
+        nombreP=input("Nombre del perro: ")
+        edadP=int(input("Edad del perro en años: "))
+        sexoP=input("Sexo del perro: ")
+        razaP=input("Raza del perro: ")
+        pesoP= float(input("Peso en kg: "))
+        estP=input("Esterización (S/N): ")
         
-        print("****************************************")
         self.DatosPerro(nombreP,edadP,sexoP,razaP,pesoP,estP)
         self.RegistroPerro()
+
         
         #funcion que contiene los datos registrados para presentar
     def MuestraDatosPerro(self):
-        print("Nombre=",self.nombre)
-        print(f"Edad={self.edad} años")
-        print("Sexo=",self.sexo)
-        print("Raza=", self.raza)
-        print(f"Peso= {self.peso} kg")
-        print("Tamaño=", self.tamaño)
-        print("Esterelizado/a=", self.esterilización)
-        print("Estado=", self.estado)
-
+        print("*****************DOCTORpet**********************")
+        print("Nombre: ",self.nombre)
+        print(f"Edad: {self.edad} años")
+        print("Sexo: ",self.sexo)
+        print("Raza: ", self.raza)
+        print(f"Peso: {self.peso} kg")
+        print("Tamaño: ", self.tamaño)
+        print("Esterelizado/a: ", self.esterilización)
+        print("Estado: ", self.estado)
+        print("*******************2024©***********************")
+        
+#objeto
 perro1=veterinaria()
 print("DATOS DEL PERRO")
+#el metodo recibe al objeto y es ejecutado sobre los datos recibidos
 perro1.RecibeDatosPerro()
+
+# el metodo muestra los datos del objeto que capturó 
 perro1.MuestraDatosPerro()
 
 
